b205f1cb4b1c25bb96aaeb32d1774140fbdf4e03
vishalkrish170-rgb <vishalkrish170@gmail.com>
2025-09-03 14:00:32 +0530

    Add files via upload

---
+++ vishal.py
@@ -0,0 +1,10 @@
+import math
+
+# Input from the user
+num = float(input("Enter a number: "))
+
+# Calculate square root
+sqrt = math.sqrt(num)
+
+# Display the result
+print("The square root of", num, "is:", sqrt)
